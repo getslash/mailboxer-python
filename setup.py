10300d735eb05bbfbc4a0e0b2493ed4b4e0f782e
Rotem Yaari <vmalloc@gmail.com>
2014-03-23 22:41:40 +0200

    Organization

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), "mailboxer-python", "__version__.py")) as version_file:
+with open(os.path.join(os.path.dirname(__file__), "mailboxer", "__version__.py")) as version_file:
     exec(version_file.read()) # pylint: disable=W0122
 
 _INSTALL_REQUIERS = []
